4eab693510741bdf642d82c48ca924a40ffdaaf9
pragkirk <pragkirk@a668f8b4-1848-0410-b2fa-1f034b7baa63>
2009-01-30 17:28:05 +0000

    added link to JSP

--- osgi/HelloWorldWebJSP/web/src/com/extensiblejava/hello/web/HelloWorldServlet.java
+++ osgi/HelloWorldWebJSP/web/src/com/extensiblejava/hello/web/HelloWorldServlet.java
@@ -11,7 +11,7 @@ public class HelloWorldServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html");
-		resp.getWriter().write("<html><body><p>Hello OSGI World -- Welcome to Jetty embedded in Felix.</p><p>Say <a href=\"./html/hello.html\"/>hello.</a></p><p>Say <a href=\"./html/goodbye.html\"/>goodbye.</a></p></body></html>"); //$NON-NLS-1$
+		resp.getWriter().write("<html><body><p>Hello OSGI World -- Welcome to Jetty embedded in Felix.</p><p>Say <a href=\"./html/hello.html\"/>hello.</a></p><p>Say <a href=\"./html/goodbye.html\"/>goodbye.</a></p><p>Serve me <a href=\"./jsp/hello.jsp\"/>a JSP please.</a></p></body></html>"); //$NON-NLS-1$
 	}
 
 }
