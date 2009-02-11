0fb86990ff37b5f8b0daeafd56ff529f45b026a0
pragkirk <pragkirk@a668f8b4-1848-0410-b2fa-1f034b7baa63>
2009-02-11 19:43:12 +0000

    Configured different messages for web vs osgi

--- osgi/HelloWorldEmbedWebJSP/web/src/com/extensiblejava/hello/web/HelloWorldServlet.java
+++ osgi/HelloWorldEmbedWebJSP/web/src/com/extensiblejava/hello/web/HelloWorldServlet.java
@@ -8,10 +8,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HelloWorldServlet extends HttpServlet {
-
+	
+	private static String source;
+	
+	public void init() throws ServletException {
+ 		source = getServletConfig().getInitParameter("source");
+	}
+	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html");
-		resp.getWriter().write("<html><body><p>Hello OSGI World -- Welcome to Jetty embedded in Felix.</p><p>Say <a href=\"./hello.html\"/>hello.</a></p><p>Say <a href=\"./goodbye.html\"/>goodbye.</a></p><p>Serve me <a href=\"./hello.jsp\"/>a JSP please.</a></p></body></html>"); //$NON-NLS-1$
+		if ( (source != null) && (source.equals("web")) ){
+			resp.getWriter().write("<html><body><p>Hello Web World -- Welcome to the Tomcat WebApp.</p><p>Say <a href=\"./hello.html\"/>hello.</a></p><p>Say <a href=\"./goodbye.html\"/>goodbye.</a></p><p>Serve me <a href=\"./hello.jsp\"/>a JSP please.</a></p></body></html>"); //$NON-NLS-1$
+		} else {
+			resp.getWriter().write("<html><body><p>Hello OSGI World -- Welcome to Equinox embedded in Tomcat.</p><p>Say <a href=\"./hello.html\"/>hello.</a></p><p>Say <a href=\"./goodbye.html\"/>goodbye.</a></p><p>Serve me <a href=\"./hello.jsp\"/>a JSP please.</a></p></body></html>"); //$NON-NLS-1$
+		}
 	}
-
 }
