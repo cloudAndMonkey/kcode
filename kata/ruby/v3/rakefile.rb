370af4d45c68f4d86a83aafb5be209d634b7319a
pragkirk <pragkirk@a668f8b4-1848-0410-b2fa-1f034b7baa63>
2009-04-09 02:40:22 +0000

    Generic path separator

--- kata/ruby/v3/rakefile.rb
+++ kata/ruby/v3/rakefile.rb
@@ -3,6 +3,6 @@ require 'rake/testtask'
 task :default => [:unit_test]
 
 Rake::TestTask.new(:unit_test) do | tsk |
- tsk.libs << "./app:./test"
+ tsk.libs << "./app" + File::PATH_SEPARATOR + "./test"
  tsk.test_files = "AllTests.rb"
 end
